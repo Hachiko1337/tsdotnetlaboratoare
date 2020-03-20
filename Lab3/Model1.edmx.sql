6b30a947cca519ff6327f0d631eff5a0a155d576
Gitea <gitea@fake.local>
2020-03-20 16:24:30 +0200

    Modified Lab2

--- Lab3/Model1.edmx.sql
+++ Lab3/Model1.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 03/03/2020 12:25:59
+-- Date Created: 03/20/2020 16:16:25
 -- Generated from EDMX file: C:\Users\hachi\source\repos\ModelDesignFirst_L1\Model1.edmx
 -- --------------------------------------------------
 
@@ -17,11 +17,23 @@ GO
 -- Dropping existing FOREIGN KEY constraints
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[FK_CustomerOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_CustomerOrder];
+GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[People]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[People];
+GO
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
+GO
+IF OBJECT_ID(N'[dbo].[Orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Orders];
+GO
 
 -- --------------------------------------------------
 -- Creating all tables
@@ -37,6 +49,24 @@ CREATE TABLE [dbo].[People] (
 );
 GO
 
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [CustomerId] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [City] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderId] int IDENTITY(1,1) NOT NULL,
+    [TotalValue] decimal(18,0)  NOT NULL,
+    [Date] datetime  NOT NULL,
+    [CustomerCustomerId] int  NOT NULL,
+    [CustomerCustomerId1] int  NOT NULL
+);
+GO
+
 -- --------------------------------------------------
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
@@ -47,9 +77,36 @@ ADD CONSTRAINT [PK_People]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
+-- Creating primary key on [CustomerId] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC);
+GO
+
+-- Creating primary key on [OrderId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderId] ASC);
+GO
+
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
+
+-- Creating foreign key on [CustomerCustomerId1] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK_CustomerOrder]
+    FOREIGN KEY ([CustomerCustomerId1])
+    REFERENCES [dbo].[Customers]
+        ([CustomerId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CustomerOrder'
+CREATE INDEX [IX_FK_CustomerOrder]
+ON [dbo].[Orders]
+    ([CustomerCustomerId1]);
+GO
 
 -- --------------------------------------------------
 -- Script has ended
---
+++ Lab3/Model1Container.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDesignFirst_L1
+{
+    public partial class Model1Container 
+    {
+        static void TesTOneToMany()
+        {
+            Console.WriteLine("One to many association");
+            using (ModelOneToManyContainer context =
+                new ModelOneToManyContainer())
+            {
+                Customer c = new Customer()
+                {
+                    Name = "Customer 1",
+                    City = "Iasi"
+                };
+                Order o1 = new Order()
+                {
+                    TotalValue = 200,
+                    Date = DateTime.Now,
+                    Customer = c
+                };
+                Order o2 = new Order()
+                {
+                    TotalValue = 300,
+                    Date = DateTime.Now,
+                    Customer = c
+                };
+                context.Customers.Add(c);
+                context.Orders.Add(o1);
+                context.Orders.Add(o2);
+                context.SaveChanges();
+                var items = context.Customers;
+                foreach (var x in items)
+                {
+                    Console.WriteLine("Customer : {0}, {1}, {2}",
+                        x.CustomerId, x.Name, x.City);
+                    foreach (var ox in x.Orders)
+                        Console.WriteLine("\tOrders: {0}, {1}, {2}",
+                            ox.OrderId, ox.Date, ox.TotalValue);
+                }
+            }
+        }
+    }
+
+}
